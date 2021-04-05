b551161a7159239d626fe5f41129f15108580a6d
JI-doo <awt0311@gmail.com>
2021-04-05 11:09:23 +0900

    [ADD] color extension

---
+++ PetsHouse_Admin/PetsHouse_Admin/Source/Extension/UIColor.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor.swift
+//  PetsHouse_Admin
+//
+//  Created by 문지수 on 2021/04/05.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(red: Int, green: Int, blue: Int, alpha: CGFloat) -> UIColor {
+        return .init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: alpha)
+    }
+    
+    static var mainColor: UIColor {
+        return rgb(red: 255, green: 153, blue: 204, alpha: 1)
+    }
+    
+    static var subColor: UIColor {
+        return rgb(red: 255, green: 157, blue: 140, alpha: 1)
+    }
+}
