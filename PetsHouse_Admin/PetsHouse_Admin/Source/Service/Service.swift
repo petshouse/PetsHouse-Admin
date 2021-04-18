f5be7aa1053bb9c75abdbd6a627fe2482f1a65e3
JI-doo <awt0311@gmail.com>
2021-04-18 13:52:44 +0900

    [ADD] signIn service

--- PetsHouse_Admin/PetsHouse_Admin/Source/Service/Service.swift
+++ PetsHouse_Admin/PetsHouse_Admin/Source/Service/Service.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+import RxSwift
+import Moya
+
+class Service {
+    let provider = MoyaProvider<AdminAPI>()
+    
+    func signIn(_ email: String, _ password: String) -> Observable<Network> {
+        return provider.rx.request(.signIn(email, password))
+            .filterSuccessfulStatusCodes()
+            .asObservable()
+            .map { _ -> Network in
+                return (.success)
+            }.catchError{ [unowned self] in return .just(self.setNetworkError($0))}
+    }
+    
+    func setNetworkError(_ error: Error) -> Network {
+        guard let status = (error as? MoyaError)?.response?.statusCode else { return (.fail) }
+         print(error)
+        return (Network(rawValue: status) ?? .fail)
+     }
+}
+
+
